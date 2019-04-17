958760a5adef5eb56017ad721fa9996c8b250905
xtlhwl <xtlhcy@outlook.com>
2019-04-17 11:14:00 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Recommend from '@/components/recommend/recommend'
+import Singer from '@/components/singer/singer'
+import Search from '@/components/search/search'
+import Rank from '@/components/rank/rank'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/recommend',
+      name: 'recommend',
+      component: Recommend
+    },
+    {
+      path: '/singer',
+      name: 'singer',
+      component: Singer
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: Search
+    },
+    {
+      path: '/rank',
+      name: 'rank',
+      component: Rank
+    }
+
+  ]
+})
