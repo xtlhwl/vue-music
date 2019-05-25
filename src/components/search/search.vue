433928b62c2fa20086e982eddaca042184101170
xtlhwl <xtlhcy@outlook.com>
2019-05-25 22:23:59 +0800

    搜索

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
-        
+        <search-box></search-box>
     </div>
 </template>
 <script>
+import SearchBox from  'base/search-box/search-box'
 export default {
-    
+    components:{
+        SearchBox
+    }
 }
 </script>
 <style lang="stylus" scoped>
